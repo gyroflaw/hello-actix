4d3439120b6a42bfe907ac773bb7b75155712e2b
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-12-27 18:07:58 +0900

    feat: add mutable app state

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
 use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use log::info;
+use std::sync::Mutex;
 
 pub struct HelloActix {
     pub port: u16,
 }
 
+struct HelloActixState {
+    app_name: String,
+    counter: Mutex<i32>,
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello(data: web::Data<HelloActixState>) -> impl Responder {
+    let app_name = &data.app_name;
+
+    let mut counter = data.counter.lock().unwrap();
+
+    *counter += 1;
+
+    HttpResponse::Ok().body(format!("Hello {app_name}!\nCounter: {counter}"))
 }
 
 #[post("/echo")]
@@ -29,6 +41,10 @@ impl HelloActix {
 
         HttpServer::new(move || {
             App::new()
+                .app_data(web::Data::new(HelloActixState {
+                    app_name: String::from("My first Actix Server"),
+                    counter: Mutex::new(0),
+                }))
                 .wrap(Logger::default())
                 .wrap(Logger::new("%a %{User-Agent}i"))
                 .service(hello)
@@ -39,5 +55,22 @@ impl HelloActix {
         .workers(8)
         .run()
         .await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{
+        http::{self},
+        test,
+    };
+
+    #[actix_web::test]
+    async fn test_hello_ok() {
+        let req = test::TestRequest::default().to_http_request();
+        let result = manual_hello().await;
+        let resp = result.respond_to(&req);
+        assert_eq!(resp.status(), http::StatusCode::OK);
     }
 }
